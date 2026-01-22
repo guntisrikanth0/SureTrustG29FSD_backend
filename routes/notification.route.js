92dcb00b102d3e3662de3e31595bc4c9b858da94
guntisrikanth0 <guntisrikanth018@gmail.com>
2026-01-22 12:48:02 +0530

    t

--- routes/notification.route.js
+++ routes/notification.route.js
@@ -1,5 +1,5 @@
-import { getNotifications, triggerLikeNotification } from "../controller/notification.controller.js";
-import exoress from 'express'
+import { getNotifications, triggerLikeNotification, markAllNotificationsAsRead, markNotificationAsRead, getUnreadNotificationCount } from "../controller/notification.controller.js";
+import express from 'express'
 import { authMiddleware } from "../utility/auth.Middleware.js";
 
 const router=exoress.Router();
@@ -7,5 +7,9 @@ const router=exoress.Router();
 
 router.post("/like",authMiddleware,triggerLikeNotification);
 router.get("/getNotifications",authMiddleware,getNotifications);
+router.get("/unreadCount",authMiddleware,getUnreadNotificationCount);
+
+router.put("/mark/:notificationId", authMiddleware, markNotificationAsRead);
+router.put("/markAll", authMiddleware, markAllNotificationsAsRead);
 
 export default router;
